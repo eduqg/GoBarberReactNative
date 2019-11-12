e5b216d26564c4c709ce3668b972a4f9b6bd90e3
eduqg <eduardoqgomes@gmail.com>
2019-11-12 17:38:39 -0300

    Bottom tab options and navigation to Profile

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import SignIn from '~/pages/SignIn';
 import SignUp from '~/pages/SignUp';
 
 import Dashboard from '~/pages/Dashboard';
+import Profile from '~/pages/Profile';
 
 // Colocar uma arrow function para receber um parâmetro que diz se está logado
 // Deste modo, é uma função que retorna um componente
@@ -16,9 +17,28 @@ export default (signedIn = false) =>
           SignIn,
           SignUp,
         }),
-        App: createBottomTabNavigator({
-          Dashboard,
-        }),
+        App: createBottomTabNavigator(
+          {
+            Dashboard,
+            Profile,
+          },
+          {
+            tabBarOptions: {
+              // Para que o teclado fique em cima na bottom bar
+              keyboardHidesTabBar: true,
+              activeTintColor: '#fff',
+              inactiveTintColor: 'rgba(255,255,255,0.6)',
+              // fundo da tab bar
+              style: {
+                backgroundColor: '#8d41a8',
+                borderTopColor: '#8d41a8',
+                borderBottomColor: '#8d41a8',
+                borderBottomWidth: 5,
+                borderTopWidth: 5,
+              },
+            },
+          }
+        ),
       },
       {
         initialRouteName: signedIn ? 'App' : 'Sign',
