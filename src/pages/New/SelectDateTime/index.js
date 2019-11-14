0f77f1ea2d9471f6da71fa0302eaddc32ee476af
eduqg <eduardoqgomes@gmail.com>
2019-11-13 22:11:52 -0300

    Datepicker for android and ios

--- src/pages/New/SelectDateTime/index.js
+++ src/pages/New/SelectDateTime/index.js
@@ -1,18 +1,32 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { TouchableOpacity, View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { MY_IP } from 'react-native-dotenv';
+import DateInput from '~/components/DateInput';
 
 import Background from '~/components/Background';
-// import { Container } from './styles';
+
+import api from '~/services/api';
+
+import { Container } from './styles';
 
 export default function SelectDateTime() {
-  return <Background />;
+  const [date, setDate] = useState(new Date());
+
+  return (
+    <Background>
+      <Container>
+        <DateInput date={date} onChange={setDate} />
+      </Container>
+    </Background>
+  );
 }
 
 SelectDateTime.navigationOptions = ({ navigation }) => ({
   title: 'Selecione o prestador',
-  // headerLeft: () => (
-  //   <TouchableOpacity onPress={() => navigation.navigate('Dashboard')}>
-  //     <Icon name="chevron-left" size={20} color="#fff" />
-  //   </TouchableOpacity>
-  // ),
+  headerLeft: () => (
+    <TouchableOpacity onPress={() => navigation.goBack()}>
+      <Icon name="chevron-left" size={20} color="#fff" />
+    </TouchableOpacity>
+  ),
 });
