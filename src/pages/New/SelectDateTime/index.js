67377e1253539ef578c27363eaedf379b3e66f3a
eduqg <eduardoqgomes@gmail.com>
2019-11-13 21:00:35 -0300

    Select provider page

--- src/pages/New/SelectDateTime/index.js
+++ src/pages/New/SelectDateTime/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import Background from '~/components/Background';
 // import { Container } from './styles';
 
 export default function SelectDateTime() {
-  return <View />;
+  return <Background />;
 }
+
+SelectDateTime.navigationOptions = ({ navigation }) => ({
+  title: 'Selecione o prestador',
+  // headerLeft: () => (
+  //   <TouchableOpacity onPress={() => navigation.navigate('Dashboard')}>
+  //     <Icon name="chevron-left" size={20} color="#fff" />
+  //   </TouchableOpacity>
+  // ),
+});
